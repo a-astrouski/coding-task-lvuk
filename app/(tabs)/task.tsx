9225f0be869fedaa3706efc0208233b712d47434
Artem Astrouski <ostrovskiy.a.97@gmail.com>
2025-08-11 23:43:33 +0200

    refactor: fix nested virtualized list error by using ParallaxFlatList optimized for list components

--- app/(tabs)/task.tsx
+++ app/(tabs)/task.tsx
@@ -1,8 +1,8 @@
-import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import { ApiService, UserDto } from '@/api';
+import ParallaxFlatList from '@/components/ParallaxFlatList';
 
 const keyExtractor = (item: UserDto) => item.id.toString();
 
@@ -21,16 +21,20 @@ export default function TabTwoScreen() {
   const renderUser = useCallback(({ item }: { item: UserDto }) => <Text>{item.name}</Text>, []);
 
   return (
-    <ParallaxScrollView
+    <ParallaxFlatList
+      keyExtractor={keyExtractor}
+      data={filteredUsers}
+      renderItem={renderUser}
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerComponent={
+        <View style={styles.container}>
+          <TextInput value={query} onChangeText={setQuery} placeholder="Search users..." />
+        </View>
+      }
       headerImage={
         <IconSymbol size={310} color="#808080" name="brain.head.profile.fill" style={styles.headerImage} />
-      }>
-      <View style={styles.container}>
-        <TextInput value={query} onChangeText={setQuery} placeholder="Search users..." />
-      </View>
-      <FlatList data={filteredUsers} keyExtractor={keyExtractor} renderItem={renderUser} />
-    </ParallaxScrollView>
+      }
+    />
   );
 }
 
